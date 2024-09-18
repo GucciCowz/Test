e23d01a72d41eb4c4e8aae8c65393c8bc4f5f9e0
GucciCowz <146901087+GucciCowz@users.noreply.github.com>
2024-09-18 18:31:14 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient("localhost", 8080);
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+        client.Close();
+    }
+}
